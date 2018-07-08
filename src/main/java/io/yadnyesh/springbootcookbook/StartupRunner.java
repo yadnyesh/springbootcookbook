2ed12509598dc50a7217bc3a9ecc64c43883c2b9
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2018-07-08 19:04:39 +0530

    Working H2 DB

--- src/main/java/io/yadnyesh/springbootcookbook/StartupRunner.java
+++ src/main/java/io/yadnyesh/springbootcookbook/StartupRunner.java
@@ -1,5 +1,6 @@
 package io.yadnyesh.springbootcookbook;
 
+import io.yadnyesh.springbootcookbook.repository.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,12 @@ public class StartupRunner implements CommandLineRunner {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private BookRepository bookRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("DataSource: " + dataSource.toString());
+		logger.info("Number of Books: "+ bookRepository.count());
 	}
 }
--- src/main/java/io/yadnyesh/springbootcookbook/entity/Book.java
+++ src/main/java/io/yadnyesh/springbootcookbook/entity/Book.java
@@ -1,9 +1,6 @@
 package io.yadnyesh.springbootcookbook.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -25,6 +22,7 @@ public class Book {
 	@ManyToOne
 	private Publisher publisher;
 	
+	@ManyToMany
 	private List<Reviewer> reviewersList;
 	
 	protected Book() {}
